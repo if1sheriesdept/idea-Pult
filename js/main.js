30345c52f50ebecfc67208efef129889276cfde1
Rahul Garg <if1sheriesdept@hotmail.com>
2014-03-02 21:32:36 +0530

    Commit shi
    First Screen

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 requirejs.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
+        jquerymmenu: '../bower_components/jquery/jquery.mmenu',
+        eiSlideShow: '../bower_components/jquery/jquery.eislideshow',
+        easing: '../bower_components/jquery/easing',
+        moveTop: '../bower_components/jquery/move-top',
         underscore: '../bower_components/underscore/underscore',
         backbone: '../bower_components/backbone/backbone',
         backboneLocalStorage: '../bower_components/backbone.localStorage/backbone.localStorage',
@@ -10,6 +14,18 @@ requirejs.config({
     shim: {
         jquery: {
             exports: '$'
+        },
+        jquerymmenu: {
+            deps: ['jquery']
+        },
+        eiSlideShow: {
+            deps: ['jquery']
+        },
+        easing: {
+            deps: ['eiSlideShow']
+        },
+        moveTop: {
+            deps: ['jquery']
         },
         underscore: {
             exports: '_',
@@ -30,13 +46,33 @@ requirejs.config({
     }
 });
 
-require([
-    'app',
-    'backbone'
-], function (app, Backbone) {
-    'use strict';
-
-    app.start();
-
-    Backbone.history.start();
+require(['jquery', 'jquerymmenu'], function($) {
+    $('nav#menu-left').mmenu();
 });
+
+require(['jquery', 'easing'], function($) {
+    $('#ei-slider').eislideshow({
+					animation			: 'center',
+					autoplay			: true,
+					slideshow_interval	: 3000,
+					titlesFactor		: 0
+                });
+});
+
+require(['jquery', 'moveTop'], function($) {
+    $(".scroll").click(function(event){		
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
+			});
+});
+
+//require([
+//    'app',
+//    'backbone'
+//], function (app, Backbone) {
+//    'use strict';
+//
+//    app.start();
+//
+//    Backbone.history.start();
+//});
